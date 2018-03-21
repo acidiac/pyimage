5da7667d597aa706d9849a0be4301841cadc9171
amit chanchal <amit.chanchal@hotmail.com>
2018-03-22 00:18:51 +0530

    take2

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import matplotlib.pylab as plt
 import numpy as np
 
+# reading the image
+img = plt.imread("fruits.jpg")
+# img.shape ==> (438, 700, 3)
+# first two dimension of this array is x and y position while third is RGB
+
+
+
+def showImage(imgarray, **kwargs):
+    """
+        plot the image
+    """
+    y = imgarray.shape[0]
+    x = imgarray.shape[1]
+  
+    plt.figure(figsize = (x,y))
+    plt.imshow(imgarray,  interpolation=None, **kwargs)
+    plt.show()
+showImage(img)
